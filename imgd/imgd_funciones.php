9e191ad6a22a8593d9b273b3443c10348acc47f7
Federico <admin@imgdigital.com.ar>
2016-09-28 12:45:57 -0300

    Otra vez sopa
    volver atrás algunos cambios

--- imgd/imgd_funciones.php
+++ imgd/imgd_funciones.php
@@ -48,13 +48,6 @@ function imgdigital_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'imgdigital_scripts' );
 
-/* Google Fonts */
-function wpb_add_google_fonts() {
-	wp_enqueue_style( 'wpb-google-fonts', '//fonts.googleapis.com/css?family=Suez-One-Regular|Italianno|Open+Sans:300,700|Open+Sans+Condensed:300,700', false );
-}
-
-add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
-
 
 /** Definir el largo de los excerpt */
 define('POST_EXCERPT_LENGTH', 55);
