a0c9da7209dddcf8e096adde412a9c1f3f3be55b
Federico <admin@imgdigital.com.ar>
2016-10-25 10:18:30 -0300

    back to the future

--- template-parts/menu-acompanan.php
+++ template-parts/menu-acompanan.php
@@ -1,17 +1,17 @@
 <h4>Acompañan este Proyecto</h4>
 <ul class="list-inline menu-acompanan">
-  <li><img width="250" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/bariloche-quiero-estar-logo-w.png" alt="Bariloche Quiero Estar Ahí">
+  <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/bariloche-quiero-estar-logo-w.png" alt="Bariloche Quiero Estar Ahí">
   </li>
-  <li><img width="250" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/gobierno-chile-turismo-logo-w.png" alt="Turismo de Gobierno de Chile">
+  <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/gobierno-chile-turismo-logo-w.png" alt="Turismo de Gobierno de Chile">
   </li>
-  <li><img width="250" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/gobierno-de-rio-negro-logo.png" alt="Gobierno de Río Negro">
+  <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/gobierno-de-rio-negro-logo.png" alt="Gobierno de Río Negro">
   </li>
-  <li><img width="250" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/municipio-chile-logo-w.png" alt="Municipio de Chile">
+  <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/municipio-chile-logo-w.png" alt="Municipio de Chile">
   </li>
-  <li><img width="250" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/region-bio-y-los-lagos-logo-w.png" alt="Región Bio y de los Lagos">
+  <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/region-bio-y-los-lagos-logo-w.png" alt="Región Bio y de los Lagos">
   </li>
-  <li><img width="250" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/turismo-chubut-logo-w.png" alt="Turismo de Chubut">
+  <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/turismo-chubut-logo-w.png" alt="Turismo de Chubut">
   </li>
-  <li><img width="250"src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/turismo-de-rio-negro-logo-w.png" alt="Turismo de Río Negro">
+  <li><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/acompanan/turismo-de-rio-negro-logo-w.png" alt="Turismo de Río Negro">
   </li>
 </ul>
