962159833dd6dff5f891ee724b18acf204adffd0
Federico <admin@imgdigital.com.ar>
2016-10-25 10:11:49 -0300

    Cambios footer 2

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 			<div class="container">
 				<div class="row">
 
-					<div class="col-md-8">
+					<div class="col-md-9">
 						<?php get_template_part('template-parts/menu', 'acompanan'); ?>
 					</div>
 
