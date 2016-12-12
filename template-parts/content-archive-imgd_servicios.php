7c072f83b2dedbf24fb71a517d0827d446a7ac1f
Federico <admin@imgdigital.com.ar>
2016-12-12 16:58:23 -0300

    Template Servicios
    Primer paso sobre el diseño

--- template-parts/content-archive-imgd_servicios.php
+++ template-parts/content-archive-imgd_servicios.php
@@ -10,7 +10,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" class="servicio col-sm-6 col-md-4">
-  <!-- <div class="thumbnail"> -->
     <?php
     // Must be inside a loop.
     if (has_post_thumbnail()) {?>
@@ -22,10 +21,9 @@
         ?>
       </a>
       <?php }
-      //echo var_dump($notitle);
 
       ?>
-      <header class="destacados-caption caption">
+      <header class="entry-header">
         <?php
         //get_the_terms(get_the_ID());
         the_title(sprintf('<h3><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h3>');
@@ -33,13 +31,25 @@
       </header><!-- .entry-header -->
 
       <div class="entry-content">
-        <?php get_datos_servicio(get_the_ID()) ?>
+        <?php $ID = get_the_ID(); ?>
+        <?php if (get_post_meta($ID , 'imgd_servicio_tel', true)){?>
+        <?php echo '<strong>Tel:</strong> '. get_post_meta($ID , 'imgd_servicio_tel', true).'<br>'; ?>
+        <?php } ?>
+        <?php if (get_post_meta($ID , 'imgd_servicio_cel', true)){?>
+        <?php echo '<strong>Cel:</strong> '. get_post_meta($ID, 'imgd_servicio_cel', true).'<br>'; ?>
+        <?php } ?>
+        <?php if (get_post_meta($ID , 'imgd_servicio_email', true)){?>
+        <?php echo '<strong>Email:</strong> <a href="mailto:'.get_post_meta($ID , 'imgd_servicio_email', true).'">'.get_post_meta($ID , 'imgd_servicio_email', true).'</a><br>'; ?>
+        <?php } ?>
+        <?php if (get_post_meta($ID , 'imgd_servicio_web', true)){?>
+        <?php echo '<strong>Web:</strong> <a href="'.get_post_meta($ID , 'imgd_servicio_web', true).'">'. get_post_meta($ID , 'imgd_servicio_web', true).'</a><br>'; ?>
+        <?php } ?>
+
+
         Direccion<br>
         ciudad - estado - pais<br>
-        tel -  cel - email
         web
       </div>
 
-    <!-- </div><!-- Thumbnail --> -->
 
   </article><!-- Col -->
