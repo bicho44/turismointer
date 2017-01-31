6d17ee3bff7b556c9b0c40dadf195161dabae856
Federico <admin@imgdigital.com.ar>
2017-01-31 09:38:39 -0300

    iconos web e email

--- template-parts/content-archive-imgd_servicios.php
+++ template-parts/content-archive-imgd_servicios.php
@@ -44,10 +44,10 @@
 
         <?php $email = get_post_meta($ID , 'imgd_servicio_email', true);
         if (!empty($email)){?>
-        <?php echo '<i class="icon-envelope-alt"></i><strong>Email:</strong> <a href="mailto:'.get_post_meta($ID , 'imgd_servicio_email', true).'">'.get_post_meta($ID , 'imgd_servicio_email', true).'</a><br>'; ?>
+        <?php echo '<a href="mailto:'.get_post_meta($ID , 'imgd_servicio_email', true).'"><i class="icon-envelope"></i></a><br>'; ?>
         <?php } ?>
         <?php if (get_post_meta($ID , 'imgd_servicio_web', true)){?>
-        <?php echo '<strong>Web:</strong> <a href="'.get_post_meta($ID , 'imgd_servicio_web', true).'">'. get_post_meta($ID , 'imgd_servicio_web', true).'</a><br>'; ?>
+        <?php echo '<strong>Web:</strong> <a href="'.get_post_meta($ID , 'imgd_servicio_web', true).'"><i class="icon-external-link"></i></a><br>'; ?>
         <?php } ?>
 
         <!-- Direccion<br>
